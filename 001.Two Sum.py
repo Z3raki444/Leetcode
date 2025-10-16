eaa63aead6d157e23e1bb44b1e7cd145c60447e5
Z3raki444 <praveenkumarm424@gmail.com>
2025-10-16 14:00:17 +0200

    Update 001.Two Sum.py

--- 001.Two Sum.py
+++ 001.Two Sum.py
@@ -1,2 +1,10 @@
 '''https://leetcode.com/problems/two-sum/description/  '''
 
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        seen = {}  # value -> index
+        for i, x in enumerate(nums):
+            need = target - x
+            if need in seen:
+                return [seen[need], i]
+            seen[x] = i
