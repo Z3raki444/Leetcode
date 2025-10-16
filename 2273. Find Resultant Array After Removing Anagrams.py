9c0b604c680ec09c54241de9af7d6d13963fd8df
Z3raki444 <praveenkumarm424@gmail.com>
2025-10-16 13:51:16 +0200

    .

---
+++ 2273. Find Resultant Array After Removing Anagrams.py
@@ -0,0 +1,13 @@
+'''https://leetcode.com/problems/find-resultant-array-after-removing-anagrams/description/'''
+
+
+class Solution:
+    def removeAnagrams(self, words: List[str]) -> List[str]:
+
+        result = [words[0]]
+
+        for i in range(1, len(words)):
+            if sorted(words[i]) != sorted(words[i - 1]):
+                result.append(words[i])
+        
+        return result
