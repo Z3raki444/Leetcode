33f391378607e29e0134189bf03e5e92651be473
Z3raki444 <praveenkumarm424@gmail.com>
2025-10-30 14:02:14 +0100

    .

--- 3370.Smallest Number With All Set Bits.py
+++ 3370.Smallest Number With All Set Bits.py
@@ -1,6 +1,20 @@
-n = 5
+'''https://leetcode.com/problems/smallest-number-with-all-set-bits/description/'''
 
-binary = bin(n)[2:]
-
-print(binary)
-print(len(set(binary)))
+class Solution:
+    def smallestNumber(self, n: int) -> int:
+       
+        binary = bin(n)[2:]
+        
+      
+        if len(set(binary)) == 1 and '1' in set(binary):
+            return n
+            
+        i = 1
+        while True:
+            m = n + i
+            binary_m = bin(m)[2:]
+           
+            if len(set(binary_m)) == 1 and '1' in set(binary_m):
+                return m
+            
+            i += 1
