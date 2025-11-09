571dd35ca9d25b7732b3b9891ed1faf42539cf3f
Z3raki444 <praveenkumarm424@gmail.com>
2025-11-09 16:22:44 +0100

    Create 2169.Count Operations to Obtain Zero.py

---
+++ 2169.Count Operations to Obtain Zero.py
@@ -0,0 +1,15 @@
+'''https://leetcode.com/problems/count-operations-to-obtain-zero/description/'''
+
+class Solution:
+    def countOperations(self, num1: int, num2: int) -> int:
+        count = 0
+        
+        while num1 and num2:
+            if num1 >= num2:
+                count += num1 // num2
+                num1 %= num2
+            else:
+                count += num2 // num1
+                num2 %= num1
+                
+        return count
